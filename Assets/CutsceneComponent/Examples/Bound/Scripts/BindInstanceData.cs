4a0fa1192e9bb203031c016c0fec06d68bbec305
Murilo Glasser <murilo.glasser@hotmail.com>
2024-03-07 12:31:21 -0300

    Mocked scene to show example

--- Assets/CutsceneComponent/Examples/Bound/Scripts/BindInstanceData.cs
+++ Assets/CutsceneComponent/Examples/Bound/Scripts/BindInstanceData.cs
@@ -9,33 +9,9 @@ public class BindInstanceData : ScriptableObject
 {
     public TimelineAsset timeline;
     public TrackGroup trackGroupType;
-    public List<Object> instances;   
-
-   /* private void CheckTimelineAsset()
-    {
-        if (timeline != null)
-        {
-            for (int i = 0; i < timeline.rootTrackCount; i++)
-            {
-                TrackAsset rootTrack = timeline.GetRootTrack(i);
-
-                if (rootTrack.name != trackGroupType.ToString())
-                    continue;
-
-                for (int j = 0; j < timeline.outputTrackCount; j++)
-                {
-                    TrackAsset outputTrack = timeline.GetOutputTrack(j);
-                    if (outputTrack.parent.name == rootTrack.name)
-                    {
-                        instances.Add(null);
-                    }
-                }
-
-            }
-
-        }
-    }*/
+    public List<Object> instances;
 }
+
 public enum TrackGroup
 {
     Trix,
@@ -45,3 +21,30 @@ public enum TrackGroup
     Duplicated
 }
 
+
+
+
+/* private void CheckTimelineAsset()
+ {
+     if (timeline != null)
+     {
+         for (int i = 0; i < timeline.rootTrackCount; i++)
+         {
+             TrackAsset rootTrack = timeline.GetRootTrack(i);
+
+             if (rootTrack.name != trackGroupType.ToString())
+                 continue;
+
+             for (int j = 0; j < timeline.outputTrackCount; j++)
+             {
+                 TrackAsset outputTrack = timeline.GetOutputTrack(j);
+                 if (outputTrack.parent.name == rootTrack.name)
+                 {
+                     instances.Add(null);
+                 }
+             }
+
+         }
+
+     }
+ }*/
